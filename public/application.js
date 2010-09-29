c8d2baa3759057c0b0bbbe5d48b0a9ef6c0f0d48
John Daniels <john@semantici.st>
2010-09-29 01:16:47 +0100

    Adding a basic tool-tip to get exact value.

--- public/application.js
+++ public/application.js
@@ -15,9 +15,46 @@ $(function () {
 					mode: "time",
 					timeformat: "%y/%m/%d"
 				},
+				grid: { hoverable: true, clickable: true },
 				lines: { show: true },
 				points: { show: true }
 			});
 		});
 	});
+
+	function showTooltip(x, y, contents) {
+		$('<div id="tooltip">' + contents + '</div>').css(
+			{
+				position: 'absolute',
+				display: 'none',
+				top: y + 5,
+				left: x + 5,
+				border: '1px solid #fdd',
+				padding: '2px',
+				'background-color': '#fee',
+				opacity: 0.80
+			}
+		).appendTo("body").fadeIn(200);
+	}
+
+	var previousPoint = null;
+	$("#graph").bind("plothover", function (event, pos, item) {
+
+		if (item) {
+			if (previousPoint != item.datapoint) {
+				previousPoint = item.datapoint;
+
+				$("#tooltip").remove();
+				var x = item.datapoint[0].toFixed(2),
+				y = item.datapoint[1].toFixed(2);
+
+				showTooltip(item.pageX, item.pageY,	item.series.label + ": &pound;" + y);
+			}
+		}
+		else {
+			$("#tooltip").remove();
+			previousPoint = null;            
+		}
+	});
+
 });
